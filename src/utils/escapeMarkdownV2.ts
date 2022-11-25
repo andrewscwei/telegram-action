70dba99d87cac4a56b49e3cbdf672e42e691f784
Andrew Wei <andrewscwei@gmail.com>
2022-11-25 13:05:33 +0100

    Fix: Escape # in markdown

--- src/utils/escapeMarkdownV2.ts
+++ src/utils/escapeMarkdownV2.ts
@@ -2,6 +2,7 @@ export default function escapeMarkdownV2(str: string): string {
   let ret = str
   ret = ret.replace(/-/g, '\\-')
   ret = ret.replace(/\./g, '\\.')
+  ret = ret.replace(/#/g, '\\#')
   ret = ret.replace(/\|/g, '\\|')
   ret = ret.replace(/>/g, '\\>')
   ret = ret.replace(/\(/g, '\\(')
