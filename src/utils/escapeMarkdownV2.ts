170ab5993b13b7910727883d92429ede9f695e70
Andrew Wei <andrewscwei@gmail.com>
2023-01-26 11:37:17 -0800

    Fix: Unescaped underscore

--- src/utils/escapeMarkdownV2.ts
+++ src/utils/escapeMarkdownV2.ts
@@ -1,6 +1,7 @@
 export default function escapeMarkdownV2(str: string): string {
   let ret = str
   ret = ret.replace(/-/g, '\\-')
+  ret = ret.replace(/_/g, '\\_')
   ret = ret.replace(/\./g, '\\.')
   ret = ret.replace(/#/g, '\\#')
   ret = ret.replace(/\|/g, '\\|')
