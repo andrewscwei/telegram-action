b9b8eca8b4f08a8c0c2d235fd1f21a6c0fd560fb
Andrew Wei <andrewscwei@gmail.com>
2026-04-24 18:00:21 -0700

    feat: Properly handle app as actor

--- src/utils/compose.ts
+++ src/utils/compose.ts
@@ -2,90 +2,6 @@ import { type Context } from './context.js'
 import { escapeMarkdownV2 } from './escapeMarkdownV2.js'
 import { type Inputs } from './inputs.js'
 
-function prefix(value?: string) {
-  if (value === undefined || value === null || value === '') return ''
-
-  return `${value} `
-}
-
-function getLabel(context: Context) {
-  if (context.eventName === 'schedule') return 'CRON'
-  if (context.ref.startsWith('refs/pull/')) return 'PR'
-
-  return 'BUILD'
-}
-
-export function composeStatus(context: Context, inputs: Inputs) {
-  const label = getLabel(context)
-  let statusStr = ''
-
-  if (inputs.isCancelled) {
-    statusStr += `${prefix(inputs.prefixes.cancelled)}*${label} CANCELLED*`
-  } else if (inputs.isSuccess) {
-    statusStr += `${prefix(inputs.prefixes.success)}*${label} PASSED*`
-  } else {
-    statusStr += `${prefix(inputs.prefixes.failure)}*${label} FAILED*`
-  }
-
-  const repoUrl = `https://github.com/${context.repo}`
-  const repoStr = `[${escapeMarkdownV2(context.repo)}](${repoUrl})`
-
-  if (context.ref.startsWith('refs/pull/')) {
-    const matches = `${context.ref}`.match(/^refs\/pull\/([^/]+)\/.*$/)
-    const prNumber = matches?.[1] ?? context.ref
-    const refStr = `[\\(${escapeMarkdownV2(`pr-#${prNumber}`)}\\)](${repoUrl}/pull/${prNumber})`
-
-    statusStr += ` in ${repoStr} ${refStr}`
-  } else {
-    const matches = `${context.ref}`.match(/^refs\/[^/]+\/(.*)$/)
-    const refName = matches?.[1] ?? context.ref
-    const refStr = `[\\(${escapeMarkdownV2(refName)}\\)](${repoUrl}/tree/${refName})`
-
-    statusStr += ` in ${repoStr} ${refStr}`
-  }
-
-  return statusStr
-}
-
-export function composeBody(context: Context, inputs: Inputs) {
-  if (!context.commitMessage) return ''
-
-  const repoUrl = `https://github.com/${context.repo}`
-  let shaStr
-
-  if (context.ref.startsWith('refs/pull/')) {
-    const matches = `${context.ref}`.match(/^refs\/pull\/([^/]+)\/.*$/)
-    const prNumber = matches?.[1] ?? context.ref
-
-    shaStr = `[\\[${context.sha.substring(0, 7)}\\]](${repoUrl}/pull/${prNumber}/commits/${context.sha})`
-  } else {
-    shaStr = `[\\[${context.sha.substring(0, 7)}\\]](${repoUrl}/commit/${context.sha})`
-  }
-
-  return `${shaStr} ${escapeMarkdownV2(context.commitMessage)}`
-}
-
-export function composeFooter(context: Context, inputs: Inputs) {
-  const actorLink = `[@${escapeMarkdownV2(context.actor)}](https://github.com/${context.actor})`
-  const workflowStr = `_${escapeMarkdownV2(context.workflow)}_`
-
-  return `${actorLink} using workflow ${workflowStr}`
-}
-
-export function composeActions(context: Context, inputs: Inputs) {
-  const repoUrl = `https://github.com/${context.repo}`
-  const jobUrl = `${repoUrl}/actions/runs/${context.runId}`
-  const buttons = []
-
-  buttons.push(`[View Job](${jobUrl})`)
-
-  if (inputs.isSuccess && inputs.action) {
-    buttons.push(`[*${escapeMarkdownV2(inputs.action.label)}*](${inputs.action.url})`)
-  }
-
-  return buttons.join(' ')
-}
-
 export function compose(context: Context, inputs: Inputs) {
   const body = composeBody(context, inputs)
   let ret = ''
@@ -101,4 +17,111 @@ export function compose(context: Context, inputs: Inputs) {
   ret += composeActions(context, inputs)
 
   return ret
+}
+
+export function composeTitle(context: Context, inputs: Inputs): string {
+  let label: string
+  switch (true) {
+    case context.eventName === 'schedule':
+      label = 'CRON'
+      break
+    case context.ref.startsWith('refs/pull/'):
+      label = 'CHECK'
+      break
+    default:
+      label = 'BUILD'
+  }
+
+  switch (true) {
+    case inputs.isCancelled: {
+      let prefix = inputs.prefixes.cancelled ?? ''
+      if (prefix) prefix += ' '
+
+      return `${prefix}${label} CANCELLED`
+    }
+    case inputs.isSuccess: {
+      let prefix = inputs.prefixes.success ?? ''
+      if (prefix) prefix += ' '
+
+      return `${prefix}${label} PASSED`
+    }
+    default: {
+      let prefix = inputs.prefixes.failure ?? ''
+      if (prefix) prefix += ' '
+
+      return `${prefix}${label} FAILED`
+    }
+  }
+}
+
+export function composeBody(context: Context, inputs: Inputs) {
+  if (!context.commitMessage) return ''
+
+  const repoURL = `https://github.com/${context.repo}`
+  let shaStr
+
+  if (context.ref.startsWith('refs/pull/')) {
+    const matches = `${context.ref}`.match(/^refs\/pull\/([^/]+)\/.*$/)
+    const prNumber = matches?.[1] ?? context.ref
+
+    shaStr = `[\\[${context.sha.substring(0, 7)}\\]](${repoURL}/pull/${prNumber}/commits/${context.sha})`
+  } else {
+    shaStr = `[\\[${context.sha.substring(0, 7)}\\]](${repoURL}/commit/${context.sha})`
+  }
+
+  return `${shaStr} ${escapeMarkdownV2(context.commitMessage)}`
+}
+
+export function composeStatus(context: Context, inputs: Inputs) {
+  let statusStr = `*${composeTitle(context, inputs)}*`
+
+  const repoURL = `https://github.com/${context.repo}`
+  const repoStr = `[${escapeMarkdownV2(context.repo)}](${repoURL})`
+
+  if (context.ref.startsWith('refs/pull/')) {
+    const matches = `${context.ref}`.match(/^refs\/pull\/([^/]+)\/.*$/)
+    const prNumber = matches?.[1] ?? context.ref
+    const refStr = `[\\(${escapeMarkdownV2(`pr-#${prNumber}`)}\\)](${repoURL}/pull/${prNumber})`
+
+    statusStr += ` in ${repoStr} ${refStr}`
+  } else {
+    const matches = `${context.ref}`.match(/^refs\/[^/]+\/(.*)$/)
+    const refName = matches?.[1] ?? context.ref
+    const refStr = `[\\(${escapeMarkdownV2(refName)}\\)](${repoURL}/tree/${refName})`
+
+    statusStr += ` in ${repoStr} ${refStr}`
+  }
+
+  return statusStr
+}
+
+export function composeFooter(context: Context, inputs: Inputs) {
+  const actorLink = `[@${escapeMarkdownV2(context.actor)}](${getActorLinkURL(context)})`
+  const workflowStr = `_${escapeMarkdownV2(context.workflow)}_`
+
+  return `${actorLink} using workflow ${workflowStr}`
+}
+
+export function composeActions(context: Context, inputs: Inputs) {
+  const repoURL = `https://github.com/${context.repo}`
+  const jobURL = `${repoURL}/actions/runs/${context.runId}`
+  const buttons = []
+
+  buttons.push(`[View Job](${jobURL})`)
+
+  if (inputs.isSuccess && inputs.action) {
+    buttons.push(`[*${escapeMarkdownV2(inputs.action.label)}*](${inputs.action.url})`)
+  }
+
+  return buttons.join(' ')
+}
+
+function getActorLinkURL(context: Context): string {
+  const botMatch = context.actor.match(/^(.+)\[bot\]$/)
+
+  if (botMatch) {
+    return `https://github.com/apps/${botMatch[1]}`
+  } else {
+    return `https://github.com/${context.actor}`
+  }
 }
--- src/utils/inputs.ts
+++ src/utils/inputs.ts
@@ -16,6 +16,37 @@ export type Inputs = {
   isSuccess: boolean
 }
 
+export function getInputs(mock?: Partial<Inputs>): Inputs {
+  const successPrefix = mock?.prefixes?.success ?? getStringInput('success-prefix', '🤖')
+  const failurePrefix = mock?.prefixes?.failure ?? getStringInput('failure-prefix', '😱')
+  const cancelledPrefix = mock?.prefixes?.cancelled ?? getStringInput('cancelled-prefix', '🫥')
+  const botToken = mock?.botToken ?? getStringInput('bot-token')
+  const chatId = mock?.chatId ?? getStringInput('chat-id')
+  const isSuccess = mock?.isSuccess ?? getBooleanInput('success', false)
+  const isCancelled = mock?.isCancelled ?? getBooleanInput('cancelled', false)
+  const actionLabel = mock?.action?.label ?? getStringInput('action-label', 'Open')
+  const actionURL = mock?.action?.url ?? getStringInput('action-url', '')
+  const hasAction = isSuccess && actionURL !== ''
+
+  return {
+    botToken,
+    chatId,
+    prefixes: {
+      cancelled: cancelledPrefix,
+      failure: failurePrefix,
+      success: successPrefix,
+    },
+    isCancelled,
+    isSuccess,
+    ...hasAction ? {
+      action: {
+        label: actionLabel,
+        url: actionURL,
+      },
+    } : {},
+  }
+}
+
 export function getStringInput(id: string, defaultValue?: string): string {
   try {
     const input = core.getInput(id, { required: true, trimWhitespace: true })
@@ -35,36 +66,5 @@ export function getBooleanInput(id: string, defaultValue?: boolean): boolean {
   } catch (err) {
     if (defaultValue !== undefined) return defaultValue
     throw Error(`Required boolean input with ID <${id}> is not provided`, { cause: err })
-  }
-}
-
-export function getInputs(mock?: Partial<Inputs>): Inputs {
-  const successPrefix = mock?.prefixes?.success ?? getStringInput('success-prefix', '🤖')
-  const failurePrefix = mock?.prefixes?.failure ?? getStringInput('failure-prefix', '😱')
-  const cancelledPrefix = mock?.prefixes?.cancelled ?? getStringInput('cancelled-prefix', '🫥')
-  const botToken = mock?.botToken ?? getStringInput('bot-token')
-  const chatId = mock?.chatId ?? getStringInput('chat-id')
-  const isSuccess = mock?.isSuccess ?? getBooleanInput('success', false)
-  const isCancelled = mock?.isCancelled ?? getBooleanInput('cancelled', false)
-  const actionLabel = mock?.action?.label ?? getStringInput('action-label', 'Open')
-  const actionUrl = mock?.action?.url ?? getStringInput('action-url', '')
-  const hasAction = isSuccess && actionUrl !== ''
-
-  return {
-    botToken,
-    chatId,
-    prefixes: {
-      cancelled: cancelledPrefix,
-      failure: failurePrefix,
-      success: successPrefix,
-    },
-    isCancelled,
-    isSuccess,
-    ...hasAction ? {
-      action: {
-        label: actionLabel,
-        url: actionUrl,
-      },
-    } : {},
   }
 }
